04b42ad6485becbcca96ea27bf95c7c417e8afb3
Jeff Geerling <geerlingguy@mac.com>
2014-10-13 14:28:03 -0500

    Actual test for XDebug.

--- tests/xdebug-test.php
+++ tests/xdebug-test.php
@@ -7,7 +7,13 @@
 
 $success = FALSE;
 
-// TODO.
+// Simple check; this function should return an empty array.
+$test = xdebug_get_code_coverage();
+if ($test === array()) {
+  $success = TRUE;
+  print "Xdebug working properly.\r\n";
+  exit(0);
+}
 
 if (!$success) {
   print "Xdebug not working properly.\r\n";
